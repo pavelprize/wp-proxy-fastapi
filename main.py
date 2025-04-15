ba9eadb9e426ec6cb9c8b606956535037b13a7ca
pavelprize <pavelprize@gmail.com>
2025-04-15 16:39:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
-import requests
+import cloudscraper
 
 app = FastAPI()
-
+scraper = cloudscraper.create_scraper()
 
 @app.post("/proxy-post")
 async def proxy_post(req: Request):
@@ -14,10 +14,17 @@ async def proxy_post(req: Request):
         payload = body["data"]
 
         headers = {"Content-Type": "application/json"}
-        response = requests.post(
-            target_url, auth=(wp_user, wp_pass), json=payload, headers=headers
+        response = scraper.post(
+            target_url,
+            auth=(wp_user, wp_pass),
+            json=payload,
+            headers=headers
         )
-        return {"status": response.status_code, "response": response.text}
+
+        return {
+            "status": response.status_code,
+            "response": response.text
+        }
 
     except Exception as e:
         return {"error": "Problem processing request", "detail": str(e)}
